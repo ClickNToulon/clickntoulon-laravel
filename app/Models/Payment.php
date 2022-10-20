9035a90c009632168c0f436cab0a4b511626f218
Quentin <quentin.boitel@outlook.fr>
2022-10-20 12:19:17 +0200

    feat(cla-24): add PHPDoc to models
    Signed-off-by: Quentin Boitel <quentin.boitel@outlook.fr>

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,17 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Defines a Payment Model. A Payment instance is a payment made by a User.
+ *
+ * @author Corentin Thibaud <corentin.thibaud@clickntoulon.fr>
+ * @author Quentin Boitel <quentin.boitel@clickntoulon.fr>
+ * @version 0.1.0
+ *
+ * @property-read int $id
+ * @property string $name
+ * @property \Illuminate\Database\Eloquent\Relations\BelongsToMany $shops
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Payment extends Model
 {
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
      * @var array<int, string>
      */
     protected $fillable = [
         'name'
     ];
 
+    /**
+     * Returns the shops that featured this Payment types.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function shops(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Shop::class);
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,17 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Defines a Status model. A status define an order state.
+ *
+ * @author Corentin Thibaud <corentin.thibaud@clickntoulon.fr>
+ * @author Quentin Boitel <quentin.boitel@clickntoulon.fr>
+ * @version 0.1.0
+ *
+ * @property-read int $id
+ * @property string $name
+ * @property \Illuminate\Database\Eloquent\Collection|null $orders
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Status extends Model
 {
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
      * @var array<int, string>
      */
     protected $fillable = [
         'name'
     ];
 
+    /**
+     * Returns the orders that has this status.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function orders(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Order::class);
