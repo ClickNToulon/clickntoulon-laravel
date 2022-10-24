c63b4181dac0d8b7950a606b8e62567e64faf2be
Quentin <quentin.boitel@outlook.fr>
2022-10-24 14:13:39 +0200

    feat(cla-28): add the new featured shops
    Add the new Featured Shops UI
    
    Signed-off-by: Quentin Boitel <quentin.boitel@outlook.fr>

--- resources/js/Components/HeroSection.vue
+++ resources/js/Components/HeroSection.vue
@@ -31,4 +31,59 @@ import { Link } from '@inertiajs/inertia-vue3';
         </div>
     </section>
     <!-- End Hero -->
+    <section class="lg:container mx-auto px-4 sm:px-6 lg:px-8 pb-32">
+        <div class="flex flex-col items-center space-y-10">
+            <h2 class="text-4xl font-semibold text-center text-slate-900">
+                Les boutiques partenaires
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-6 w-full mx-auto cols">
+                <div class="col-span-1 relative">
+                    <Link href="" class="absolute inset-0 hover:bg-black/5 rounded-xl"/>
+                    <img src="https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="rounded-xl" alt="shop image">
+                    <div class="p-4 space-y-2">
+                        <div class="flex justify-between items-center">
+                            <h3 class="text-lg font-bold tracking-wider text-slate-900">
+                                Boulangerie du coin
+                            </h3>
+                            <div class="space-x-1 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <span class="text-sm font-medium text-slate-900">
+                                    2 Avenue de la République 83000 Toulon
+                                </span>
+                            </div>
+                        </div>
+                        <p class="text-slate-900 truncate">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-span-1 relative">
+                    <Link href="#" class="absolute inset-0 hover:bg-black/5 rounded-xl"/>
+                    <img src="https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="rounded-xl" alt="shop image">
+                    <div class="p-4 space-y-2">
+                        <div class="flex justify-between items-center">
+                            <h3 class="text-lg font-bold tracking-wider text-slate-900">
+                                Boulangerie du coin
+                            </h3>
+                            <div class="space-x-1 inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <span class="text-sm font-medium text-slate-900">
+                                    2 Avenue de la République 83000 Toulon
+                                </span>
+                            </div>
+                        </div>
+                        <p class="text-slate-900 truncate">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
