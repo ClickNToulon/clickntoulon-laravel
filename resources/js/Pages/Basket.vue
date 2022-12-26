054c23ef8fe8566f3fcdffd342788dfb4c5c7e11
Quentin <quentin.boitel@outlook.fr>
2022-12-26 22:26:28 +0100

    feat: remove unused icon in alert message
    Signed-off-by: Quentin Boitel <quentin.boitel@outlook.fr>

--- resources/js/Pages/Basket.vue
+++ resources/js/Pages/Basket.vue
@@ -50,9 +50,6 @@ const alert = ref(true);
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
 					</svg>
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-					</svg>
 					<!-- text -->
 					<span class="ml-2 text-green-500 font-medium">{{ $page.props.flash.success }}</span>
 				</div>
