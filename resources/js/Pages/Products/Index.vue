7d6810fa388c853b2ddb99dc0f5b086d1381383f
Quentin <quentin.boitel@outlook.fr>
2022-12-29 17:28:40 +0100

    feat: filter products by min and max price based whether they are on sale or not
    Signed-off-by: Quentin Boitel <quentin.boitel@outlook.fr>

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -8,12 +8,15 @@ import Checkbox from "@/Components/Form/Checkbox.vue";
 import PrimaryButton from "@/Components/Button/PrimaryButton.vue";
 import ProductCard from "@/Components/Product/ProductCard.vue";
 import {Inertia} from "@inertiajs/inertia";
+import TextInput from "@/Components/Form/TextInput.vue";
 
 defineProps({
     products: Object,
 	types: Object,
     productType: Object,
 	onSale: String,
+	minPrice: Number,
+	maxPrice: Number,
 });
 
 const ad = ref(true);
@@ -45,39 +48,48 @@ function resetForm() {
 										Tout effacer
 									</button>
 								</div>
-								<div class="flex flex-col h-[190px] overflow-y-auto w-full custom-scrollbar">
-									<div class="flex items-center space-x-4 w-full" v-for="type in types">
+								<div class="flex flex-col max-h-[190px] overflow-y-auto w-full custom-scrollbar">
+									<div class="flex items-center w-full" v-for="type in types">
 										<label class="flex items-center mb-1" :for="'type-' + type.id">
-											<Checkbox name="types[]" :value="type.id" id="type-1" :checked="productType.includes(String(type.id))" class="border-2 focus:ring-0" />
-											<span class="ml-4 text-slate-700 font-medium truncate w-full">{{ type.name }}</span>
+											<Checkbox name="types[]" :value="type.id" :id="'type-' + type.id" :checked="productType.includes(String(type.id))" class="border-2 focus:ring-0" />
+											<span class="ml-2 text-slate-700 font-medium truncate w-full">{{ type.name }}</span>
 										</label>
 									</div>
 								</div>
 							</div>
-							<!--<div class="flex-col items-center justify-between">
+							<div class="flex-col items-center justify-between">
 								<div class="flex items-center justify-between mb-2">
 									<div class="flex items-center">
 										<h3 class="text-sm uppercase tracking-wide font-semibold text-slate-900">
-											Prix
+											Prix (en euros)
 										</h3>
 									</div>
 								</div>
-								<div class="flex flex-col w-full"></div>
-							</div>-->
-							<div class="flex items-center space-x-4 w-full">
+								<div class="flex flex-col w-full">
+									<div class="flex items-center gap-2">
+										<div>
+											<TextInput id="min_price" name="min_price" type="number" :value="minPrice" class="mt-1 block w-full" min="0" placeholder="Min..." />
+										</div>
+										<div>
+											<TextInput id="max_price" name="max_price" type="number" :value="maxPrice" class="mt-1 block w-full" placeholder="Max..." />
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="flex items-center w-full">
 								<label class="flex items-center">
 									<Checkbox name="on_sale" id="on_sale" class="border-2 focus:ring-0" :checked="onSale === 'on'" />
-									<span class="ml-4 text-slate-700 font-medium truncate w-full">Uniquement les produits en promotion</span>
+									<span class="ml-2 text-slate-700 font-medium truncate w-full">Uniquement les produits en promotion</span>
 								</label>
 							</div>
-							<PrimaryButton class="w-full !mt-4" type="submit">Appliquer les filtres</PrimaryButton>
+							<PrimaryButton class="w-full" type="submit">Appliquer les filtres</PrimaryButton>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 		<!-- Vetical line -->
-		<div class="hidden xl:block w-1 mt-4 h-[518px] bg-slate-200"></div>
+		<div class="hidden xl:block w-px mt-4 h-[518px] bg-slate-900/10"></div>
 		<!-- Sidebar with filters -->
 		<div class="w-full pt-8 xl:pl-8">
 			<div class="w-full">
@@ -95,9 +107,9 @@ function resetForm() {
 					<div class="col-span-1 relative" v-for="product in products.data" :key="product.id" v-if="products.total !== 0">
 						<ProductCard :product="product" />
 					</div>
-					<div class="col-span-3 flex flex-col items-center justify-center text-xl font-semibold" v-else>
-						<span>Aucun produit ne correspond à votre recherche ou aux filtres sélectionnés.</span>
-						<span>Veuillez réessayer en modifiant votre recherche ou les filtres aplliqués.</span>
+					<div class="col-span-3 space-y-2 flex flex-col" v-else>
+						<span class="text-2xl font-medium">Aucun produit ne correspond à votre recherche ou aux filtres sélectionnés.</span>
+						<span class="text-xl">Veuillez réessayer en modifiant votre recherche ou les filtres aplliqués.</span>
 					</div>
 				</div>
 				<div class="mx-auto w-full flex justify-center">
